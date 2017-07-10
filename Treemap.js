2d8d499e266346665fe08ae51757aacccc8e54c4
annyrtd <fyz95@mail.ru>
2017-07-10 14:37:59 +0300

    back to the older version

--- Treemap.js
+++ Treemap.js
@@ -29,30 +29,61 @@ class Treemap {
 }
 
     static function setUpTreemap(settings, index) {
-    var rowHeaderTitles = context.report.TableUtils.GetRowHeaderCategoryTitles(settings.tableName);
+    //var rowHeaderTitles = context.report.TableUtils.GetRowHeaderCategoryTitles(settings.tableName);
     var rowHeaderIds = context.report.TableUtils.GetRowHeaderCategoryIds(settings.tableName);
     var valueColumnIndex = settings.valueColumnIndex ? settings.valueColumnIndex : 1;
     var rowHeaderValues = context.report.TableUtils.GetColumnValues(settings.tableName, valueColumnIndex);
     var colorValues = settings.colorValueColumnIndex ? context.report.TableUtils.GetColumnValues(settings.tableName, settings.colorValueColumnIndex) : rowHeaderValues;
-    var rowheaders = [];
 
-    for(var i = 0; i < rowHeaderIds.length; i++) {
+    //var rowheaders = [];
+
+    /*for(var i = 0; i < rowHeaderIds.length; i++) {
         rowheaders[rowHeaderIds[i][0]] = {
             index: i,
             title: rowHeaderTitles[i][0],
             value: rowHeaderValues[i].Value,
             colorValue: colorValues[i].Value
         };
-    }
+    }*/
+
+    //var codes = table.GetColumnValues("id");
+    //var names = table.GetColumnValues("__l9");
+    //var parents = table.GetColumnValues(settings.parentColumn);
+
 
     var schema = context.confirmit.GetDBDesignerSchema(settings.schemaId);
     var table = schema.GetDBDesignerTable(settings.databaseTableName);
-    var codes = table.GetColumnValues("id");
-    var names = table.GetColumnValues("__l9");
-    var parents = table.GetColumnValues(settings.parentColumn);
+    var dataTableRows = table.GetDataTable().Rows;
+    var idColumnName = "id";
+    var labelColumName = "__l9";
+    var parentColumnName = settings.parentColumn;
     var categoriesArray = [];
 
-    for(var i=0; i<codes.Count; i++){
+    for(var i = 0; i < rowHeaderIds.length; i++) {
+
+        var id = rowHeaderIds[i][0].toLowerCase();
+        var searchFunction = function(item) {
+            return item[idColumnName].toLowerCase() === id;
+        };
+        var row = findItem(dataTableRows, searchFunction);
+        var parent = row[parentColumnName];
+        var value = rowHeaderValues[i].Value;
+        var colorValue = colorValues[i].Value;
+        var name = row[labelColumName];
+
+        categoriesArray.push({
+            id: id,
+            name: name,
+            parent: parent,
+            index: i,
+            value: value,
+            colorValue: colorValue
+        });
+    }
+
+
+    context.log.LogDebug('test12');
+    /*for(var i=0; i<codes.Count; i++){
         categoriesArray.push({
             id: codes.Item(i),
             name: names.Item(i),
@@ -61,14 +92,23 @@ class Treemap {
             value: rowheaders[codes.Item(i)].value,
             colorValue: rowheaders[codes.Item(i)].colorValue
         });
-    }
+    }*/
 
     var text = context.component;
+
+    context.log.LogDebug('test12.1');
     var colorFunctionName = "colorFunction_" + index;
+
+    context.log.LogDebug('test12.2');
     var categoriesArrayName = "categoriesArray" + index;
+
+    context.log.LogDebug('test12.3');
     text.Output.Append(JSON.print(categoriesArray, categoriesArrayName));
+
+    context.log.LogDebug('test12.4');
     text.Output.Append(JSON.print(settings.colorFunction, colorFunctionName));
 
+    context.log.LogDebug('test13');
     var str = "var treemap_" + index +
         " = new Reportal.ZoomableTreemap({tableContainerId: '" + settings.tableContainerId + "'," +
         "treemapContainerId: '" + settings.treemapContainerId + "', " +
@@ -79,6 +119,18 @@ class Treemap {
         (settings.title ? ("title: '" + settings.title + "', ") : "") +
         "flatHierarchy: " + categoriesArrayName + "});";
 
+    context.log.LogDebug('test14');
     text.Output.Append("<script>" + str + "</script>");
+    context.log.LogDebug('test15');
 }
+
+    static function findItem(arr, condition) {
+        for (var i = 0; i < arr.Count; i++) {
+            if(condition(arr[i])) {
+                return arr[i];
+            }
+        }
+
+        return null;
+    }
 }
